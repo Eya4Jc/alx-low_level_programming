c7233568d4fd22c635899cd5aaaab0a9d21d5850
Eya4JC <eyasutariku@gmail.com>
2022-07-06 07:43:00 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /*
- * 
- *
  * * _isalpha - checks for alphabetical letters
  *
  * * @c: a charter to be checke on
  *
- * * Return: returns 0 or 1 depending on condition
- *
- * 
+ * * Return: returns 0 or 1 depending on condition 
  */
 int _isalpha(int c)
 {
