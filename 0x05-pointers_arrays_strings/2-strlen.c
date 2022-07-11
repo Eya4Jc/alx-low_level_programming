7ebf044b243fd776b8476a13674373d8436eb448
Eya4JC <eyasutariku@gmail.com>
2022-07-11 06:27:16 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strln -length of a string
+ * 0s: a pointer to an int that will be changed
+ * 
+ * Return: void that means ans is correct
+ */
+int strln(char *s);
+{
+int u;
+u=0;
+while(s[u] != '/0')
+{
+u++
+}
+return (u);
+}
+
