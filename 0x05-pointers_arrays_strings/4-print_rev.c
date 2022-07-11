2ec1c4d1ed28df557ff8c263ca2a05f056ecb879
Eya4JC <eyasutariku@gmail.com>
2022-07-11 13:19:44 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,9 +3,9 @@
  * print_rev -this function prints reversing string
  * @s :this is input string
  */
-void print_rev(char *s);
+void Print_rev(char *s);
 int i;
-for (i = 0; s[i] != '\0'; i++);
+for (i = 0; s[i] != '\0'; i++)
 for (i = i - 1; s[i] != '\0'; i--)
 {
 _putchar(s[i]);
