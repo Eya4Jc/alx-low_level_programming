51d0d0fb3ba4e8aa9a0ab721ff7951ddaaf38d9d
Eya4JC <eyasutariku@gmail.com>
2022-07-11 05:56:41 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap-int swaps the values of two intiger
+ * using two input parameter
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *  return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *n;
+*a = *b;
+*b = temp;
+}
